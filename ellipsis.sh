7d83b11b33cb404c9987649428dc6d53c7229257
jdollar <dollarhidejoel@gmail.com>
2017-10-03 20:56:07 -0400

    use a directory to pull private profile and aliases

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 #
 # jdollar/dot-zsh ellipsis package
+pkg.install() {
+  mkdir -p $HOME/.config/zsh_profile.d
+  mkdir -p $HOME/.config/zsh_alias.d
+}
 
 pkg.link() {
   case $(os.platform) in
