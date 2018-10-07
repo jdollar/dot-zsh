bef2c5110878fca835b06228f36f4c542d6b9cd3
jdollar <dollarhidejoel@gmail.com>
2018-10-06 20:01:52 -0400

    Adding linux files and updating creating d folders

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 #
 # jdollar/dot-zsh ellipsis package
+
+CONFIG_DIR="$HOME/.config"
+PROFILE_D_DIR="$CONFIG_DIR/zsh_profile.d"
+ALIAS_D_DIR="$CONFIG_DIR/zsh_alias.d"
+ENV_D_DIR="$CONFIG_DIR/zsh_env.d"
+
+D_DIRS=array( "$PROFILE_D_DIR" "$ALIAS_D_DIR" "$ENV_D_DIR" )
+
 pkg.install() {
-  mkdir -p $HOME/.config/zsh_profile.d
-  mkdir -p $HOME/.config/zsh_alias.d
-  mkdir -p $HOME/.config/zsh_env.d
+  for i in "${D_DIRS[@]}"
+  do
+    [[ ! -d "$i" ]] && mkdir -p $i
+  done
 }
 
 pkg.link() {
   case $(os.platform) in
     osx)
       fs.link_files platforms/osx
+      ;;
+    linux)
+      fs.link_files platforms/linux
       ;;
   esac
 }
